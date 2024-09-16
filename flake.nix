0a59c94acd896574270c7867a745a3cdf359f356
PukNgae Cryolitia <cryolitia@project-trans.org>
2024-09-17 02:33:10 +0800

    ci: fix magic-nix-cache
    Signed-off-by: PukNgae Cryolitia <cryolitia@project-trans.org>

--- flake.nix
+++ flake.nix
@@ -4,16 +4,20 @@
     {
       experimental-features = [ "nix-command" "flakes" ];
       substituters = [
-        # "https://mirrors.cernet.edu.cn/nix-channels/store"
-        # "https://mirrors.bfsu.edu.cn/nix-channels/store"
-        "https://cache.nixos.org/"
-      ];
-      extra-substituters = [
-        "https://cryolitia.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "cryolitia.cachix.org-1:/RUeJIs3lEUX4X/oOco/eIcysKZEMxZNjqiMgXVItQ8="
-      ];
+      # "https://mirrors.cernet.edu.cn/nix-channels/store"
+      # "https://mirrors.bfsu.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+
+      "https://cryolitia.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cryolitia.cachix.org-1:/RUeJIs3lEUX4X/oOco/eIcysKZEMxZNjqiMgXVItQ8="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
     };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
